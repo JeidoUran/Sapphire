6843f1ff40c898f09b7be8c27753f332e33733fd
JeidoUran <tourais.q@gmail.com>
2019-04-08 16:17:34 +0200

    Start changing world id to Ragnarok
    cba to change from Light to Chaos everytime I boot retail

--- src/world/Actor/Player.cpp
+++ src/world/Actor/Player.cpp
@@ -2031,7 +2031,7 @@ void Sapphire::Entity::Player::setLandFlags( uint8_t flagSlot, uint32_t landFlag
 {
   m_landFlags[ flagSlot ].landIdent = ident;
   // todo: leave this in for now but we really need to handle this world id shit properly
-  m_landFlags[ flagSlot ].landIdent.worldId = 67;
+  m_landFlags[ flagSlot ].landIdent.worldId = 97;
   m_landFlags[ flagSlot ].landFlags = landFlags;
   m_landFlags[ flagSlot ].unkown1 = 0;
 }
