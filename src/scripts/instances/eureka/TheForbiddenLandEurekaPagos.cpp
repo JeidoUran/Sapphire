8d92507a94bc5c5cf75b24d2c65712ba18e5b56d
JeidoUran <tourais.q@gmail.com>
2019-04-19 02:09:41 +0200

    Preparing eobj for Pagos

---
+++ src/scripts/instances/eureka/TheForbiddenLandEurekaPagos.cpp
@@ -0,0 +1,39 @@
+#include <ScriptObject.h>
+#include <Territory/PublicContent.h>
+#include <Actor/Player.h>
+
+using namespace Sapphire;
+
+class TheForbiddenLandEurekaPagos : public Sapphire::ScriptAPI::PublicContentScript
+{
+public:
+  TheForbiddenLandEurekaPagos() : Sapphire::ScriptAPI::PublicContentScript( 4 )
+  { }
+
+  void onPlayerSetup( Sapphire::PublicContent& instance, Entity::Player& player )
+  {
+    player.getCurrentZone()->setWeatherOverride( static_cast< Common::Weather >( 2 ) );
+    player.setRot( -2.2499f );
+    player.setPos( { 719.924f, -802.56f, 397.794f } );
+  }
+
+  void onInit( PublicContent& instance ) override
+  {
+//RP Added eobj
+    instance.registerEObj( "baportal", 2009726, 0, 4, { 213.954f, 757.882f, -937.514f }, 1.000000f, -0.00524855f ); 
+  }
+
+  void onUpdate( PublicContent& instance, uint32_t currTime ) override
+  {
+
+  }
+
+  void onEnterTerritory( PublicContent& instance, Entity::Player& player, uint32_t eventId, uint16_t param1,
+                         uint16_t param2 ) override
+  {
+
+  }
+
+};
+
+EXPOSE_SCRIPT( TheForbiddenLandEurekaPagos );
