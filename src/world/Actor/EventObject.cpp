a8a59bae3cff89de3b88536a17c2b985fddca4fb
JeidoUran <tourais.q@gmail.com>
2019-05-10 14:22:41 +0200

    Disable spawning/despawning eobj logging

--- src/world/Actor/EventObject.cpp
+++ src/world/Actor/EventObject.cpp
@@ -126,7 +126,7 @@ void Sapphire::Entity::EventObject::spawn( Sapphire::Entity::PlayerPtr pTarget )
   if( !pTarget->isObjSpawnIndexValid( spawnIndex ) )
     return;
 
-  Logger::debug( "[{0}] Spawning EObj: id#{1} name={2}", pTarget->getId(), getId(), getName() );
+  // Logger::debug( "[{0}] Spawning EObj: id#{1} name={2}", pTarget->getId(), getId(), getName() );
 
   auto eobjStatePacket = makeZonePacket< FFXIVIpcObjectSpawn >( getId(), pTarget->getId() );
   eobjStatePacket->data().spawnIndex = spawnIndex;
@@ -146,7 +146,7 @@ void Sapphire::Entity::EventObject::spawn( Sapphire::Entity::PlayerPtr pTarget )
 
 void Sapphire::Entity::EventObject::despawn( Sapphire::Entity::PlayerPtr pTarget )
 {
-  Logger::debug( "[{0}] despawn eobj#{1}", pTarget->getId(), getId() );
+  // Logger::debug( "[{0}] despawn eobj#{1}", pTarget->getId(), getId() );
 
   pTarget->freeObjSpawnIndexForActorId( getId() );
 }
