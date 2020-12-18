410f9000da2e5dd1c3d57338952d12f2f5228312
Jeido <tourais.q@gmail.com>
2020-12-18 23:02:04 +0100

    Implement moogle setup

---
+++ src/scripts/custom/CtsEtcVoyagerMoogle.cpp
@@ -0,0 +1,35 @@
+#include <ScriptObject.h>
+#include <Actor/Player.h>
+
+#include <Exd/ExdDataGenerated.h>
+#include <Service.h>
+
+
+using namespace Sapphire;
+
+class CtsEtcVoyagerMoogle :
+  public Sapphire::ScriptAPI::EventScript
+{
+private:
+  constexpr static auto ACTION_ATTUNE = 0x13;
+  constexpr static auto ACTION_TELEPORT = 0x4;
+
+  constexpr static auto AETHERYTE_MENU_AETHERNET = 1;
+  constexpr static auto AETHERYTE_MENU_HOUSING = 2;
+  constexpr static auto AETHERYTE_MENU_HOME_POINT = 3;
+  constexpr static auto AETHERYTE_MENU_FAVORITE_POINT = 4;
+  constexpr static auto AETHERYTE_MENU_FAVORITE_POINT_SECURITY_TOKEN = 5;
+
+public:
+  CtsEtcVoyagerMoogle() :
+    Sapphire::ScriptAPI::EventScript( 0xB0236 )
+  {
+  }
+
+void onTalk( uint32_t eventId, Entity::Player& player, uint64_t actorId ) override
+  {
+	for( uint32_t i = 5728; i < 5813; i++ )
+	  player.addItem( i, 5, false, false, true, true );
+  }
+};
+EXPOSE_SCRIPT( CtsEtcVoyagerMoogle );
