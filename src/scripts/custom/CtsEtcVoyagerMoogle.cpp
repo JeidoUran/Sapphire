57ee9e6ade04677280083118ac7b0f88f46854d4
Jeido Uran <tourais.q@gmail.com>
2020-12-22 01:28:28 +0100

    Moogles in starting cities auto complete certain quests when talked to
    For now, auto completes the quests Shadowbringers and Hope's Confluence in order to bypass the weather checks in ShB zones (Everlasting Light and Starshower)

--- src/scripts/custom/CtsEtcVoyagerMoogle.cpp
+++ src/scripts/custom/CtsEtcVoyagerMoogle.cpp
@@ -28,8 +28,10 @@ public:
 
 void onTalk( uint32_t eventId, Entity::Player& player, uint64_t actorId ) override
   {
-	for( uint32_t i = 5728; i < 5813; i++ )
-	  player.addItem( i, 5, false, false, true, true );
+	// for( uint32_t i = 5728; i < 5813; i++ )
+	  // player.addItem( i, 5, false, false, true, true );
+	player.finishQuest( 69190 );
+	player.finishQuest( 69314 );
   }
 };
 EXPOSE_SCRIPT( CtsEtcVoyagerMoogle );
